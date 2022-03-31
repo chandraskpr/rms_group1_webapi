9bd5f338296d696ecb410e741c316496d80b0ada
Rahul Mishra <rahul.mishra@thepsi.com>
2022-03-31 22:38:44 +0530

    Variable name of UserNotificationDomain and UserNotification made similar

---
+++ RmsWebApi/RmsWebApi/Controllers/ResumeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RmsWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResumeController : ControllerBase
+    {
+
+    }
+}
---
+++ RmsWebApi/RmsWebApi/Data/UserNotificationsDomain.cs
@@ -0,0 +1,17 @@
+﻿using RmsWebApi.RMS_DB;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RmsWebApi.Data
+{
+    public partial class UserNotification
+    {
+        public int NotificationId { get; set; }
+        public int UserId { get; set; }
+        public string NotificationDescription { get; set; } = null!;
+        public byte[] CreationDate { get; set; } = null!;
+        public string? NotificationState { get; set; }
+
+        public virtual UserInfo User { get; set; } = null!;
+    }
+}
