799aca8063528a0e7b175b626e7d1c648190fe74
Rahul Mishra <rahul.mishra@thepsi.com>
2022-04-05 12:01:51 +0530

    File name changed to make it different from the file names of entity files.

---
+++ RmsWebApi/RmsWebApi/Data/AboutMes.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RMS.Domain.ResumeDomain
+{
+    public class AboutMes
+    {
+        public string MainDescription { get; set; }
+        public string KeyPoints { get; set; }
+    }
+}
