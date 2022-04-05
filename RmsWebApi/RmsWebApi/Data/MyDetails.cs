3fa8279602ace46e6d034cb9c57d04cc0c3e5210
Rahul Mishra <rahul.mishra@thepsi.com>
2022-04-05 12:43:08 +0530

    Data type changed to make is similar with data type of entity

--- RmsWebApi/RmsWebApi/Data/MyDetails.cs
+++ RmsWebApi/RmsWebApi/Data/MyDetails.cs
@@ -5,6 +5,6 @@ namespace RMS.Domain.ResumeDomain
     public class MyDetails
     {
         public string ProfilePicture { get; set; }  
-        public float TotalExp { get; set; }
+        public double? TotalExp { get; set; }
     }
 }
