e9124f5be15c774449e77229796ebb442f61793a
Rahul Mishra <rahul.mishra@thepsi.com>
2022-04-05 12:04:11 +0530

    Domain file changed because I had added deatils of child tables of resume table

--- RmsWebApi/RmsWebApi/Data/AboutMe.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-namespace RMS.Domain.ResumeDomain
-{
-    public class AboutMe
-    {
-        public string MainDescription { get; set; }
-        public string KeyPoints { get; set; }
-    }
-}
