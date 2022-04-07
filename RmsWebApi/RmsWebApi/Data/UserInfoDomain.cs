f8acbfce54a8d15f0dccaea3469742acd9b7f930
Devang Vashistha <devang.vashistha@thepsi.com>
2022-04-07 17:16:11 +0530

    Added UserInfoDomain Properties

--- RmsWebApi/RmsWebApi/Data/UserInfoDomain.cs
+++ RmsWebApi/RmsWebApi/Data/UserInfoDomain.cs
@@ -1,19 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+
+
 namespace RmsWebApi.Data
 {
     public class UserInfoDomain
+
+
+
     {
-        [Key]
+        public UserInfoDomain()
+        {
+            userResumeDomains = new List<UserResumeDomain>();
+        }
+        
         public int UserId { get; set; }
         
-        [Required]
         public string UserName { get; set; }
-
-        [Required]
+        
         public string UserRole { get; set; }
-
-        [Required]
+       
         public string UserEmail { get; set; }
+        public List<UserResumeDomain> userResumeDomains { get; set; }
     }
 }
+
+
