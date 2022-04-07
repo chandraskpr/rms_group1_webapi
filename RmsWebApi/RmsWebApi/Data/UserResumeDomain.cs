f2815c44a2f0a85e7248dc533307fcfdc6f556a7
Devang Vashistha <devang.vashistha@thepsi.com>
2022-04-07 17:24:43 +0530

    Updated

---
+++ RmsWebApi/RmsWebApi/Data/UserResumeDomain.cs
@@ -0,0 +1,20 @@
+﻿using RmsWebApi.RMS_DB;
+using System;
+using System.Collections.Generic;
+
+
+
+namespace RmsWebApi.Data
+{
+    public partial class UserResumeDomain
+    {
+        public int UserResumeId { get; set; }
+        public int? UserId { get; set; }
+        public int ResumeId { get; set; }
+
+
+
+        //public virtual Resume Resume { get; set; } = null!;
+        //public virtual UserInfo? User { get; set; }
+    }
+}
