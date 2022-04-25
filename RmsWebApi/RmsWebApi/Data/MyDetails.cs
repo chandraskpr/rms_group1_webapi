c26235da91df8f9f8c4d87c9773fd9a71f71f0d8
Rahul Mishra <rahul.mishra@thepsi.com>
2022-04-25 23:08:56 +0530

    Datatype of profile picture changed temproraly. In database datatype is changed to varbinary(max> from varchar(100).

--- RmsWebApi/RmsWebApi/Data/MyDetails.cs
+++ RmsWebApi/RmsWebApi/Data/MyDetails.cs
@@ -4,7 +4,7 @@ namespace RMS.Domain.ResumeDomain
 {
     public class MyDetails
     {
-        public string ProfilePicture { get; set; }  
+        public byte[] ProfilePicture { get; set; }  
         public double? TotalExp { get; set; }
         public string UserName { get; set; }
         public string Role { get; set; }
